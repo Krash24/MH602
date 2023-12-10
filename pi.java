a8afae3c4c938554af7fc909d3ab22ac09b2372a
Krash24 <151226213+Krash24@users.noreply.github.com>
2023-12-10 14:46:17 +0000

    added features

--- pi.java
+++ pi.java
@@ -1,16 +1,19 @@
 public class pi {
-    public static void main(String[]args) {
-        int num1 = 5; 
-        int num2 = 10; 
-        int num3 = 15; 
-        int result1 = 0; 
-        int result2 = 0; 
-        int result3 = 0; 
-        result1 = num1 + num2 + num3; 
-        result2 = num1 * num2; 
-        result3 = num3 / num1; 
-        System.out.println("The answer for " + num1 + num2 + num3 + " is " + result1); 
-        System.out.println("The answer for " + num1 + num2 + " is " + result2); 
-        System.out.println("The answer for " + num1 + num3 + " is " + result3);
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 15;
+        int result1 = 0;
+        int result2 = 0;
+        int result3 = 0;
+        int result4 = 0;
+        result1 = num1 + num2 
+        result2 = num1 * num2;
+        result3 = num3 / num1;  
+        result4 = num3 - num2;
+        System.out.println("The answer for " + num1 + " + "  +  num2 + " + " +  num3 + " is " + result1); 
+        System.out.println("The answer for " + num1 + " * " +  num2 + " is " + result2); 
+        System.out.println("The answer for " + num1 + " / " +  num3 + " is " + result3);
+        System.out.println("The answer for " + num3 +  " - " +  num2 + " is " + result4);
     }
 }
